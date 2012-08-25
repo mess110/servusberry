44cfeeb7b3bc7de5be78418155045c4c932dbc63
Cristian Mircea Messel <mess110@gmail.com>
2012-08-25 10:12:11 +0300

    initial install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install update
+sudo apt-get install upgrade
+
+sudo apt-get install transmission-daemon git-core python-pip
+
+git clone git@github.com:mess110/servusberry.git
+cd servusberry
