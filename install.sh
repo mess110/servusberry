f1114c35ef1d2aad98ec9641706f753e0b887890
Cristian Mircea Messel <mess110@gmail.com>
2012-08-25 10:19:27 +0300

    say yes to apt-get install prompt

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 # sudo apt-get update
 # sudo apt-get upgrade
 
-sudo apt-get install transmission-daemon git-core python-pip
+sudo apt-get -y install transmission-daemon git-core python-pip
 
 git clone git@github.com:mess110/servusberry.git
 cd servusberry
