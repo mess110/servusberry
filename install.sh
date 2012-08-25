aa786c033aa3027675163f113781a5a977c6ae01
Cristian Mircea Messel <mess110@gmail.com>
2012-08-25 11:22:10 +0300

    initial flask app

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-# sudo apt-get update
-# sudo apt-get upgrade
+# sudo apt-get -y update
+# sudo apt-get -y upgrade
 
 sudo apt-get -y install transmission-daemon git-core python-pip
 
+sudo pip install Flask transmissionrpc
+
 git clone git@github.com:mess110/servusberry.git
 cd servusberry
+
+
