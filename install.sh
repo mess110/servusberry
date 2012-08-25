6235ddd4df71712be831badddd52bf6e46a8a023
Cristian Mircea Messel <mess110@gmail.com>
2012-08-25 12:29:16 +0300

    install from readonly

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 # sudo apt-get -y update
 # sudo apt-get -y upgrade
 
-sudo apt-get -y install transmission-daemon git-core python-pip
+# sudo apt-get -y install transmission-daemon git-core python-pip
 
-sudo pip install Flask transmissionrpc
+# sudo pip install Flask transmissionrpc
 
-git clone git@github.com:mess110/servusberry.git
+git clone git://github.com/mess110/servusberry.git
 cd servusberry
-
-
