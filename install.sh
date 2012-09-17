01e8e1e1c146a61f6e165b9447deca3b507ead9d
Cristian Mircea Messel <cristian.messel@smarttech2000.eu>
2012-09-17 16:55:07 +0300

    fix install script

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
-# sudo apt-get -y update
-# sudo apt-get -y upgrade
+sudo apt-get -y update
 
-# sudo apt-get -y install transmission-daemon git-core python-pip vim mpg123
+sudo apt-get -y install transmission-daemon git-core python-pip vim mpg123
 
-# sudo pip install Flask transmissionrpc
-
-git clone git://github.com/mess110/servusberry.git
-cd servusberry
+sudo pip install Flask transmissionrpc sh
