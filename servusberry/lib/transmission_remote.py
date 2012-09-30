f579ac4fc28632c5ba05e477eaa50c74068961b8
Cristian Messel <mess110@gmail.com>
2012-09-30 22:36:44 +0300

    easier to iterate over torrents in java

--- servusberry/lib/transmission_remote.py
+++ servusberry/lib/transmission_remote.py
@@ -2,17 +2,18 @@ import transmissionrpc
 
 def list_torrents():
   tc = transmissionrpc.Client('127.0.0.1')
-  result = {}
+  result = []
 
   for key in tc.list():
     torrent = tc.info(key)[1]
-    result[key] = {
+    result.append({
       'name': torrent.name,
+      'key': key,
       'progress': torrent.progress,
       'eta': torrent.format_eta(),
       'status': torrent.status,
       'hashString': torrent.hashString,
       'files': torrent.files()
-    }
+    })
 
   return result
