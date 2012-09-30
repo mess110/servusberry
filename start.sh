af716f22ab28c864207f12cd68177d74c23acd38
Cristian Messel <mess110@gmail.com>
2012-09-30 23:08:01 +0300

    fix start script

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
+SCRIPT_PATH=`readlink -f $0`
+SCRIPT_DIR=`dirname $SCRIPT_PATH`
+
 killall -9 transmission-deamon
 transmission-daemon --no-blocklist --no-auth --allowed="192.168.*.*, 127.0.0.1"
 
-python `pwd`/app.py &
+python $SCRIPT_DIR/app.py &
