317925e21e77e42b03cf85d9e9ff6733f4126e06
Cristian Messel <mess110@gmail.com>
2012-09-30 22:55:12 +0300

    update start script

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-# killall -9 transmission-deamon
-# transmission-deamon
-python app.py &
+killall -9 transmission-deamon
+transmission-daemon --no-blocklist --no-auth --allowed="192.168.*.*, 127.0.0.1"
+
+python `pwd`/app.py &
