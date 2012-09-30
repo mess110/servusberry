437652bc7febbe774913140ed134928ef9b3f53f
Cristian Messel <mess110@gmail.com>
2012-09-30 23:19:10 +0300

    fix start script

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 SCRIPT_PATH=`readlink -f $0`
 SCRIPT_DIR=`dirname $SCRIPT_PATH`
 
-killall -9 transmission-deamon
+sudo killall -9 /usr/bin/transmission-daemon
+killall -9 transmission-daemon
 transmission-daemon --no-blocklist --no-auth --allowed="192.168.*.*, 127.0.0.1"
 
 python $SCRIPT_DIR/app.py &
